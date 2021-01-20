f478ec589810ecbe58dbe8386442d6856eba4042
Chanti36 <71897938+Chanti36@users.noreply.github.com>
2021-01-20 12:55:29 +0100

    Add files via upload

---
+++ Game_Scene.cpp
@@ -0,0 +1,144 @@
+/*
+ * GAME SCENE
+ * Copyright © 2018+ Ángel Rodríguez Ballesteros
+ *
+ * Distributed under the Boost Software License, version  1.0
+ * See documents/LICENSE.TXT or www.boost.org/LICENSE_1_0.txt
+ *
+ * angel.rodriguez@esne.edu
+ */
+
+#include "Game_Scene.hpp"
+
+#include <cstdlib>
+#include <basics/Canvas>
+#include <basics/Director>
+
+using namespace basics;
+using namespace std;
+
+namespace example
+{
+
+    Game_Scene::Game_Scene()
+    {
+        // Se establece la resolución virtual (independiente de la resolución virtual del dispositivo).
+        // En este caso no se hace ajuste de aspect ratio, por lo que puede haber distorsión cuando
+        // el aspect ratio real de la pantalla del dispositivo es distinto.
+
+        canvas_width  = 1280;
+        canvas_height =  720;
+
+        // Se inicia la semilla del generador de números aleatorios:
+
+        srand (unsigned(time(nullptr)));
+
+        // Se inicializan otros atributos:
+
+        initialize ();
+    }
+
+
+
+    bool Game_Scene::initialize ()
+    {
+        state     = LOADING;
+        suspended = true;
+        gameplay  = UNINITIALIZED;
+
+        return true;
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    void Game_Scene::suspend ()
+    {
+        suspended = true;               // Se marca que la escena ha pasado a primer plano
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    void Game_Scene::resume ()
+    {
+        suspended = false;              // Se marca que la escena ha pasado a segundo plano
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    void Game_Scene::handle (Event & event)
+    {
+        if (state == RUNNING)               // Se descartan los eventos cuando la escena está LOADING
+        {
+            if (gameplay == WAITING_TO_START)
+            {
+                start_playing ();           // Se empieza a jugar cuando el usuario toca la pantalla por primera vez
+            }
+            else switch (event.id)
+            {
+                case ID(touch-started):     // El usuario toca la pantalla
+                case ID(touch-moved):
+                {
+                    user_target_y = *event[ID(y)].as< var::Float > ();
+                    follow_target = true;
+                    break;
+                }
+
+                case ID(touch-ended):       // El usuario deja de tocar la pantalla
+                {
+                    follow_target = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    void Game_Scene::update (float time)
+    {
+        if (!suspended) switch (state)
+        {
+            case LOADING: load_textures  ();     break;
+            case RUNNING: run_simulation (time); break;
+            case ERROR:   break;
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    void Game_Scene::render (Context & context)
+    {
+        if (!suspended)
+        {
+            // El canvas se puede haber creado previamente, en cuyo caso solo hay que pedirlo:
+
+            Canvas * canvas = context->get_renderer< Canvas > (ID(canvas));
+
+            // Si no se ha creado previamente, hay que crearlo una vez:
+
+            if (!canvas)
+            {
+                 canvas = Canvas::create (ID(canvas), context, {{ canvas_width, canvas_height }});
+            }
+
+            // Si el canvas se ha podido obtener o crear, se puede dibujar con él:
+
+            if (canvas)
+            {
+                canvas->clear ();
+
+                switch (state)
+                {
+                    case LOADING: render_loading   (*canvas); break;
+                    case RUNNING: render_playfield (*canvas); break;
+                    case ERROR:   break;
+                }
+            }
+        }
+    }
+
+
+
+
+
+}
